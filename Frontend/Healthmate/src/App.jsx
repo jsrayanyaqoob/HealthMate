76b38be148b0b1d9097a6dde8af9a3ebac08c40e
jsrayanyaqoob <rayanyaqoob83@gmail.com>
2025-10-20 19:19:41 +0500

    bug fixed

--- Frontend/Healthmate/src/App.jsx
+++ Frontend/Healthmate/src/App.jsx
@@ -1,19 +1,18 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "../components/Home";
 import Login from "../components/Login";
 import Signup from "../components/Signup";
 import Dashboard from "../components/Dashboard";
-import Report from "../components/Feedback";
 
-export default function App() {
+function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/report" element={<Report />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
   );
 }
+
+export default App;
